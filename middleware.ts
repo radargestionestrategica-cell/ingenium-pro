6486d4bd6a65513c53caa11081274fa80a0ecbf9
unknown <TU_EMAIL_ACA@ejemplo.com>
2026-04-08 23:18:30 -0300

    proteccion total activa

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,8 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+export default clerkMiddleware((auth, req) => {
+    auth().protect();
+  });
 
 export const config = {
   matcher: [
