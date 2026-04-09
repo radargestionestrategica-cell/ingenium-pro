e8fd68a7c0f606f4daab83fac0d56b15a3e298cd
unknown <TU_EMAIL_ACA@ejemplo.com>
2026-04-08 21:17:54 -0300

    layout con codigo de seguridad

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import { ClerkProvider } from '@clerk/nextjs'
 import './globals.css'
 
 export default function RootLayout({
@@ -10,14 +10,6 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="es">
         <body>
-          <header style={{ display: 'flex', justifyContent: 'flex-end', padding: '1rem' }}>
-            <SignedOut>
-              <SignInButton mode="modal" />
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-          </header>
           {children}
         </body>
       </html>
