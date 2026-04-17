b80b6a9a81bc4d7c861926fabf4d8bdab8fa0cf6
unknown <TU_EMAIL_ACA@ejemplo.com>
2026-04-17 17:57:02 -0300

    fix: prisma lazy init

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,9 @@
 import { PrismaClient } from '@prisma/client';
 
-declare global {
-  var prisma: PrismaClient | undefined;
-}
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-export const db = global.prisma || new PrismaClient();
+export const db = globalForPrisma.prisma ?? new PrismaClient({
+  datasources: { db: { url: process.env.DATABASE_URL } }
+});
 
-if (process.env.NODE_ENV !== 'production') {
-  global.prisma = db;
-}
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db;
