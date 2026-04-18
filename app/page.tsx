1178d154cd59f3b8baa7e3a6a29b4a20fdffe67b
unknown <TU_EMAIL_ACA@ejemplo.com>
2026-04-18 00:37:22 -0300

    fix: dynamic after use client

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-export const dynamic = 'force-dynamic';
 'use client';
+export const dynamic = 'force-dynamic';
 import ModuloPerforacion from '@/components/ModuloPerforacion';
 import { useState, useRef, useEffect } from 'react';
 
